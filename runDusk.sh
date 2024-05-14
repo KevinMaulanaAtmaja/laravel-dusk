bca5bdf3cd06e862eaa1c29d0cf85c84cc829ee0
Kevin Maulana Atmaja <j04719671@gmail.com>
2024-05-14 17:26:21 +0700

    update runDusk.sh

--- runDusk.sh
+++ runDusk.sh
@@ -3,6 +3,11 @@ php artisan dusk --filter=CheckAuthTest
 php artisan dusk --filter=CheckFormTest  
 php artisan dusk --filter=CheckCrudTest  
 
+# installation guide
+# php artisan dusk:install
+# create =>  .env.dusk.local
+# APP_URL =>  http://127.0.0.1:8001
+
 # run serve with diff port
 # php artisan serve --port=8001 --env=dusk.local
 # php artisan migrate:fresh --seed
